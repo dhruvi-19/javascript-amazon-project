9c481b9a30aee3438a69a3297d7331b1084a6c3d
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-08 14:06:24 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -117,7 +117,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
          });
      }
 
+     cart.forEach(() => {
 
-     console.log(cart);
+     });
+
+     let cartQuantity = 0;
+
+     cart.forEach((item) => {
+        cartQuantity += item.quantity;
+     });
+
+     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
