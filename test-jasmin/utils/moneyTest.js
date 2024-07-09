34c5c7aa0647f417310808a37014fdb376168e2a
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-09 12:59:00 +0530

    Create automated tests for addToCart

--- test-jasmin/utils/moneyTest.js
+++ test-jasmin/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('Test suit : formateCurrency', () => {
     it('converts cents into dollars' , () => {
