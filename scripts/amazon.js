5228384138e0243ccd727fe5c24f5f8857909f64
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-09 16:57:41 +0530

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,11 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid(renderProductsGrid){
+
 
 let productsHTML = '';
 
@@ -83,3 +88,4 @@ document.querySelectorAll('.js-add-to-cart')
       updateCartQuantity();
     });
   });
+}
