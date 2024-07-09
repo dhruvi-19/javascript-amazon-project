11990e40d271fbb7e35206410a1266246f5d568f
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-09 13:50:54 +0530

    Create automated test for order summary

--- data/cart.js
+++ data/cart.js
@@ -24,25 +24,27 @@ function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
   }
 
-export function addToCart(productId){
+  export function addToCart(productId) {
     let matchingItem;
-
+  
     cart.forEach((cartItem) => {
-       if(productId === cartItem.productId){
-           matchingItem = cartItem;
-       }
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
     });
-
-    if(matchingItem){
-       matchingItem.quantity += 1;
+  
+    if (matchingItem) {
+      matchingItem.quantity += 1;
     } else {
-       cart.push({
-           productId : productId,
-           quantity : 1,
-           deliveryOptionId : '1'
-        });
+      cart.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: '1'
+      });
     }
-}
+  
+    saveToStorage();
+  }
 
 export function removeFromCart(productId) {
     const newCart = [];
