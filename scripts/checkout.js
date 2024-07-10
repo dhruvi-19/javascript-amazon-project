986ca34b678dfd20dddf1fc0de7f91ad1bc8a6db
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-10 11:10:41 +0530

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,15 +4,31 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 import '../data/backend-practice.js';
-import { loadProducts, loadProductsFetch } from "../data/products.js";
+import {  loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
-Promise.all([
-  new Promise((resolve) =>{
-    loadProducts(() => {
-      resolve('value1');
+async function loadPage(){
+  
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
     });
-  }),
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+
+
+}
+loadPage();
+
+
+
+/*
+Promise.all([
+ loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
@@ -23,6 +39,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) =>{
